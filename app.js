7350ac59854784b282cb4ee6d518e250a5b409a9
Kevin Blicharski <kevin-blicharski@uiowa.edu>
2018-10-21 01:21:00 -0500

    Added a point @yabsi

--- app.js
+++ app.js
@@ -141,6 +141,15 @@ app.get('/fields', function (req, res) {
     });
 });
 
+app.get('/field_details', function (req, res) {
+    const farm_id = req.query.farm_id;
+    oAuthSession.get(`https://sandboxapi.deere.com:443/platform/organizations/223031/fields/${farm_id}/boundaries`, 'e857ab85-d146-4d38-b566-7c4e6cff79bd', 'Vng+hvL4t0toSM8kBApJILa4LjgRfEbLDs6Wk1syew6n/OfmxWrRBDThrTGIDAIr5VB3w5uVvV7irvhqo/tpmiPb7x6GJmds9h7XdsZF5no=', function (error, responseData, result) {
+        console.log('StatusCode => ' + result.statusCode);
+        console.log('----- Sample Request Response -----');
+
+        res.send(JSON.stringify(JSON.parse(responseData), null, 2));
+    });
+});
 
 app.listen(3000);
 
